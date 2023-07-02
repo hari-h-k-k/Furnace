da426198108189a47a9b8caa42a118b1d1a79527
SuryaSnG <surya.thottathil.2001@gmail.com>
2023-07-02 16:56:28 +0100

    Unfinished

--- furnace/src/App.js
+++ furnace/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Home from './components/home/Home';
+
 function App() {
-  return (
-    <Home/>
-  );
+    return (
+        <Home/>
+    );
 }
 
 export default App;
--- furnace/src/components/home/Home.js
+++ furnace/src/components/home/Home.js
@@ -1,21 +1,68 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../../App.css';
 
-function Home() {
-  const circleContainers = [];
-  for (let i = 1; i <= 100; i++) {
-    circleContainers.push(
-      <div className="circle-container" key={i}>
-        <div className="circle"></div>
-      </div>
-    );
-  }
+const containerHeight = 400;
+const circleRadius = 20;
 
-  return (
-    <div className="container">
-      {circleContainers}
-    </div>
-  );
+function Home() {
+
+    const [circleY, setCircleY] = useState(circleRadius);
+    const [circleVelocity, setCircleVelocity] = useState(5);
+    const [circleOpacity, setCircleOpacity] = useState(1);
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCircleY((prevY) => {
+                const newY = prevY + circleVelocity;
+                const newOpacity = newY > containerHeight ? 0 : 1;
+                return newY > containerHeight ? -circleRadius : newY;
+            });
+            setCircleOpacity((prevOpacity) => {
+                return circleY > containerHeight ? 1 : prevOpacity;
+            });
+        }, 16); // 60 FPS
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [circleY, circleVelocity]);
+
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCircleY((prevY) => {
+                const newY = prevY + circleVelocity;
+                return newY > containerHeight ? -circleRadius : newY;
+            });
+        }, 16); // 60 FPS
+
+        setCircleOpacity(circleY > containerHeight ? 0 : 1);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [circleVelocity, circleY, circleOpacity]);
+
+
+
+    return (
+        <div className="container" style={{ "--circle-radius": `${circleRadius}px` }}>
+            <div
+                className="circle"
+                style={{ top: `${circleY}px`, opacity: circleOpacity }}
+            />
+        </div>
+        // <div className="container">
+        //     {Array.from({ length: 1 }, (_, i) => (
+        //         <div className="circle-container" key={i + 1}>
+        //             <div className="circle"></div>
+        //         </div>
+        //     ))}
+        // </div>
+    );
 }
+
 
 export default Home;
