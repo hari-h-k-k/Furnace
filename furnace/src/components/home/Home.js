ec5ea7ea4a65568b52750804d69e3166f0ec9af4
Harikrishnan K <harikdas2001@gmail.com>
2023-07-02 22:43:27 +0530

    cf bg

--- furnace/src/components/home/Home.js
+++ furnace/src/components/home/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../../App.scss';
-
+import StartUp from '../../asset/video/StartUp.mp4';
 function Home() {
   const circleContainers = [];
-  for (let i = 1; i <= 100; i++) {
+  for (let i = 1; i <= 200; i++) {
     circleContainers.push(
       <div className="circle-container" key={i}>
         <div className="circle"></div>
@@ -13,14 +13,22 @@ function Home() {
 
   return (
     <>
-    <div className="container">
-      {circleContainers}
-    </div>
-    
-    <div style={{background:"green", height:'100px'}}>
+      <div className="container">
+        {circleContainers}
+      </div>
 
-    </div>
-    
+      <div style={{ background: "green", height: '10vh' }}>
+
+      </div>
+
+      <video autoPlay loop muted style={{ height: '90vh', width: '100%', objectFit: 'cover', marginBottom: '-10px' }}>
+        <source src={StartUp} />
+      </video>
+
+      <p style={{height: '100vh' }}>
+        Dummy
+      </p>
+      
     </>
   );
 }
