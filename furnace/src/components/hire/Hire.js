e87a6ccddb042b24fc39cc516f454f1e7930adc4
SuryaSnG <surya.thottathil.2001@gmail.com>
2023-07-04 19:09:43 +0100

    Contact us Parked

--- furnace/src/components/hire/Hire.js
+++ furnace/src/components/hire/Hire.js
@@ -3,6 +3,15 @@ import React, {useEffect, useState} from 'react';
 function Hire() {
 
     const [isMobile, setIsMobile] = useState(false);
+    const [hover, setHover] = useState(false);
+
+    const handleMouseEnter = () => {
+        setHover(true);
+    };
+
+    const handleMouseLeave = () => {
+        setHover(false);
+    };
 
     useEffect(() => {
         const handleResize = () => {
@@ -26,7 +35,7 @@ function Hire() {
         leftDiv: {
             flex: 1,
             color: 'white',
-            paddingBlock: isMobile ? '10vh' : '15vh',
+            paddingBlockStart: isMobile ? '10vh' : '15vh',
         },
         rightDiv: {
             flex: 1,
@@ -42,18 +51,25 @@ function Hire() {
             marginRight: '45px',
         },
         field: {
-            // display: 'flex',
-            // flexDirection: 'row',
-            // placeContent: 'start',
-            marginBottom: '1vh',
+            marginBottom: '2vh',
         },
         input: {
-            marginLeft: '1vh',
+            // marginLeft: '1vh',
             borderRadius: '25px',
             height: '40px',
             width: '60%',
             background: '-webkit-gradient(linear, center top, center bottom, color-stop(0%, #ccc), color-stop(100%, #fff))',
             paddingInline: '10px',
+        },
+        nameInput: {
+            marginLeft: '8px',
+            fontFamily: 'cursive',
+            fontSize: '2vh'
+        },
+        emailInput: {
+            marginLeft: '13px',
+            fontFamily: 'cursive',
+            fontSize: '2vh'
         },
         messageLabel: {
             fontFamily: 'cursive',
@@ -64,13 +80,28 @@ function Hire() {
             marginLeft: '1vh',
             borderRadius: '25px',
             height: '20vh',
-            width: '50%',
+            width: '60%',
             background: '-webkit-gradient(linear, center top, center bottom, color-stop(0%, #ccc), color-stop(100%, #fff))',
             verticalAlign: 'top',
             padding: '10px',
+            fontFamily: 'cursive',
+            fontSize: '2vh'
         },
-        formGroup: {
-
+        submitButton: {
+            borderWidth: '3px',
+            borderStyle: 'solid',
+            borderColor: hover ? '#000000' : '#ffffff',
+            fontFamily: 'cursive',
+            color: hover ? '#000000' : '#ffffff',
+            height: '80px',
+            width: '18vh',
+            borderRadius: '20px',
+            fontSize: 'xx-large',
+            marginTop: '30px',
+            cursor: hover ? 'pointer' : '',
+            background: hover
+                ? '-webkit-gradient(linear, center top, center bottom, color-stop(0%, #ccc), color-stop(100%, #fff))'
+                : '-webkit-gradient(linear, center top, center bottom, color-stop(0%, #930505), color-stop(100%, #000000))',
         },
         submit: {
             textAlign: 'center',
@@ -119,7 +150,7 @@ function Hire() {
                                 <input
                                     type="text"
                                     name="name"
-                                    style={styles.input}
+                                    style={{ ...styles.input, ...styles.nameInput }}
                                     value={formState.name}
                                     onChange={handleChange}
                                     required
@@ -130,7 +161,7 @@ function Hire() {
                                 <input
                                     type="email"
                                     name="email"
-                                    style={styles.input}
+                                    style={{ ...styles.input, ...styles.emailInput }}
                                     value={formState.email}
                                     onChange={handleChange}
                                     required
@@ -148,7 +179,7 @@ function Hire() {
                             </div>
                         </div>
                         <div style={styles.submit}>
-                            <button style={{cursor: 'pointer'}} type="submit">Submit</button>
+                            <button style={styles.submitButton} type="submit" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Submit</button>
                         </div>
                     </form>
                 </div>
