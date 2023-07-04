c67b85d1a6af960b64a1be6eeb4da7fa20153bb6
SuryaSnG <surya.thottathil.2001@gmail.com>
2023-07-04 21:57:10 +0100

    Tilt

--- furnace/src/components/about/Card.js
+++ furnace/src/components/about/Card.js
@@ -1,41 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Tilt from 'react-vanilla-tilt';
+import './About.css';
 
 function Card({item}) {
 
-    return (
-        <>
-            <div style={styles.card}>
-                <h2 style={styles.heading}>{item.heading}</h2>
-                <p style={styles.data}>
-                    {item.discription}
-                </p>
-            </div>
-        </>
-    );
-}
+    const [hover, setHover] = useState(false);
 
-const styles = {
-    card: {
-        height: '300px',
-        width: '400px',
-        // backgroundColor: 'green',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        textAlign: 'center',
-        padding: '10px',
-        border: '1px solid #ccc',
-    },
-    heading: {
-        fontFamily: 'cursive',
-        fontSize: '24px',
-        color: 'white',
-    },
-    data: {
-        fontFamily: 'cursive',
-        fontSize: '16px',
-        color: 'white',
-    },
-};
+    const handleMouseEnter = () => {
+        setHover(true);
+    };
+
+    const handleMouseLeave = () => {
+        setHover(false);
+    };
+
+    const styles = {
+        card: {
+            height: '300px',
+            width: '400px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            textAlign: 'center',
+            padding: '10px',
+            borderWidth: hover ? '5px' : '',
+            borderStyle: hover ? 'solid' : '',
+            borderColor: hover ? '#ffffff' : '',
+            borderRadius: hover ? '15px' : '',
+            margin: hover ? '5px' : '10px',
+        }, heading: {
+            fontFamily: 'cursive', fontSize: '24px', color: 'white', fontsize: 'xxx-large'
+        }, data: {
+            fontFamily: 'cursive', fontSize: '16px', color: 'white',
+        },
+    };
+
+
+    return (<>
+        <Tilt className="custom-tilt" style={{background: ''}} onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}>
+            <div style={styles.card}>
+                <h1 style={styles.heading}>{item.heading}</h1>
+                <p style={styles.data}>{item.description}</p>
+            </div>
+        </Tilt>
+    </>);
+}
 
 export default Card;
