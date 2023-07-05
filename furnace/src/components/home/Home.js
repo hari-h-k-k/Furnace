1572cd37cff1b0547616c8bb4d43c34075d26b25
SuryaSnG <surya.thottathil.2001@gmail.com>
2023-07-05 11:15:02 +0100

    Clean 2

--- furnace/src/components/home/Home.js
+++ furnace/src/components/home/Home.js
@@ -6,7 +6,6 @@ import Footer from '../footer/Footer';
 import Bg from '../bg/Bg';
 import Hero from '../hero/Hero';
 import Hire from '../hire/Hire';
-import Workflow from '../workflow/Workflow';
 
 function Home() {
 
