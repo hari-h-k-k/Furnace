26f221651a05335ac57695c0c40e924990b9f2e2
SuryaSnG <surya.thottathil.2001@gmail.com>
2023-07-07 18:09:23 +0100

    About us Raw Fix - Best

--- furnace/src/components/about/Card.js
+++ furnace/src/components/about/Card.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Tilt from 'react-vanilla-tilt';
 import './About.css';
 
 function Card({item}) {
 
+    const [hover, setHover] = useState(false);
+
+    const handleMouseEnter = () => {
+        setHover(true);
+    };
+
+    const handleMouseLeave = () => {
+        setHover(false);
+    };
+
     return (
         <>
-            <div className="card">
-                <h2 className="heading">{item.heading}</h2>
-                <p className="data">
-                    {item.description}
-                </p>
-            </div>
+            <Tilt className="custom-tilt" style={{background: ''}} onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}>
+                <div className="card">
+                    <h2 className="heading">{item.heading}</h2>
+                    <p className="data">
+                        {item.description}
+                    </p>
+                </div>
+            </Tilt>
         </>
     );
 }
