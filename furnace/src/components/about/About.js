513132644f01698308624282de449d02c9c8c3f1
SuryaSnG <surya.thottathil.2001@gmail.com>
2023-07-07 17:08:12 +0100

    About us Raw Fix

--- furnace/src/components/about/About.js
+++ furnace/src/components/about/About.js
@@ -1,25 +1,16 @@
 import Card from "./Card";
 import aboutContent from '../../constants/About.json'
+import './About.css';
+
 function About() {
 
     return (
-        <div style={styles.gridLayout}>
+        <div className="gridLayout">
             {aboutContent.map( (item,index) => (
                 <Card item={item}/>
             ))}
-
         </div>
     );
 }
 
 export default About;
-
-const styles = {
-    gridLayout: {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fit, minmax(400px, 1fr))',
-        gap: '1rem',
-        placeItems:'center',
-        margin:'1rem'
-    }
-};
--- furnace/src/components/about/Card.js
+++ furnace/src/components/about/Card.js
@@ -1,40 +1,18 @@
 import React from 'react';
+import './About.css';
 
 function Card({item}) {
 
     return (
         <>
-            <div style={styles.card}>
-                <h2 style={styles.heading}>{item.heading}</h2>
-                <p style={styles.data}>
+            <div className="card">
+                <h2 className="heading">{item.heading}</h2>
+                <p className="data">
                     {item.description}
                 </p>
             </div>
         </>
     );
 }
-
-const styles = {
-    card: {
-        height: '300px',
-        width: '400px',
-        // backgroundColor: 'green',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        textAlign: 'center',
-        padding: '10px'
-    },
-    heading: {
-        fontFamily: 'sans-serif',
-        fontSize: '24px',
-        color: 'white',
-    },
-    data: {
-        fontFamily: 'Your Attractive Font',
-        fontSize: '16px',
-        color: 'white',
-    },
-};
 
 export default Card;
