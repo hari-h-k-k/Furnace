dabc75f030470280540e24854b0e028974f202ca
SuryaSnG <surya.thottathil.2001@gmail.com>
2023-07-07 09:24:40 +0100

    Find video shrinking point

--- furnace/src/components/Intro/Intro.js
+++ furnace/src/components/Intro/Intro.js
@@ -1,20 +1,45 @@
+import React, { useEffect, useState } from 'react';
 import StartUp from '../../asset/video/StartUp.mp4';
 import './Intro.css';
+
 function Intro() {
 
+    useEffect(() => {
+        function checkVideoCrop() {
+            const videoElement = document.querySelector('.video');
+            const screenWidth = window.outerWidth;
+            console.log("Screen" + screenWidth)
+            const videoWidth = videoElement.offsetWidth;
+            console.log("Video" + videoWidth)
+            const isCropped = videoWidth > screenWidth;
+            if (isCropped) {
+                console.log('Video is cropped!');
+            } else {
+                console.log('Video is not cropped.');
+            }
+        }
+
+        checkVideoCrop();
+        window.addEventListener('resize', checkVideoCrop);
+        return () => {
+            window.removeEventListener('resize', checkVideoCrop);
+        };
+    }, []);
+
     return (
-        <div className={"videoContainer"}>
-            <video className={"video"} autoPlay loop muted>
+        <div className="videoContainer">
+            <video className="video" autoPlay loop muted>
                 <source src={StartUp} />
             </video>
-            <div className={"textOverlay"}>
-                <h1 className={"text"}>Drive Your Recognition</h1>
+            <div className="textOverlay">
+                <h1 className="text">Drive Your Recognition</h1>
             </div>
         </div>
     );
 }
 
-export default Intro
+export default Intro;
+
 
 
 // import StartUp from '../../asset/video/StartUp.mp4';
