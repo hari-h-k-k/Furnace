32043ead8eef86cbb5c42bfd185bd18fd3b4a7b0
SuryaSnG <surya.thottathil.2001@gmail.com>
2023-07-09 18:41:17 +0100

    Conflict Resolved

--- furnace/src/components/header/Navbar.js
+++ furnace/src/components/header/Navbar.js
@@ -7,7 +7,6 @@ import './Navbar.css';
 function Navbar() {
     const location = useLocation();
     const handleScroll = (ele) => {
-        // First navigate to navigate('/');
         const element = document.getElementById(ele);
         element.scrollIntoView({behavior: 'smooth'});
     };
@@ -34,11 +33,11 @@ function Navbar() {
                 <div className="logoContainer">
                     <img className="logo" src={Logo} alt='Furnace' onClick={handleHome} />
                 </div>
-                {/*<div className="buttonContainer">*/}
-                {/*    <button className="navButton" onClick={handleHome}>Home</button>*/}
-                {/*    <button className="navButton" onClick={() => {handleScroll('aboutUs')}}>About Us</button>*/}
-                {/*    <button className="navButton" onClick={handleWorkflow}>Business</button>*/}
-                {/*</div>*/}
+                <div className="buttonContainer">
+                    <button className="navButton" onClick={handleHome}>Home</button>
+                    <button className="navButton" onClick={() => {handleScroll('aboutUs')}}>About Us</button>
+                    <button className="navButton" onClick={handleWorkflow}>Business</button>
+                </div>
             </div>
         </>
     );
